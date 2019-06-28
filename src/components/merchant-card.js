3a05f532ee5741b9c3516a0aa1d6748459d6480b
mohammad ahsan <40141811+ahsan8244@users.noreply.github.com>
2019-06-29 02:40:27 +0800

    implemented FlatList for food merchants

---
+++ src/components/merchant-card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View, Image } from 'react-native'
+
+class MerchantCard extends React.Component {
+    render(){
+        return(
+            <View style={{ height: 180, flex: 1, borderWidth: 0.5, borderColor: '#dddddd', marginHorizontal: 20, marginTop: 20 }}>
+                <View style={{ flex: 3 }}>
+                    <Image source={this.props.imageUri}
+                    style={{ resizeMode: 'cover', flex: 1, width: null, height: null }}/>
+                </View>
+                <View style={{ flex: 1, paddingTop: 12, paddingHorizontal: 12}}>
+                    <Text>{this.props.title}</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default MerchantCard;
--- src/screens/food.js
+++ src/screens/food.js
@@ -1,15 +1,43 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native'
+import MerchantCard from '../components/merchant-card'
 
 
 class Food extends React.Component {
-  render(){
-    return (
-      <View style={styles.container}>
-        <Text>Food</Text> 
-      </View>
-    )
-  }
+    constructor(props){
+        super(props)
+        this.state = {
+            data: [
+                {
+                    imageUrl: require('../../assets/splash.png'),
+                    title: 'McDonalds'
+                },
+                {
+                    imageUrl: require('../../assets/splash.png'),
+                    title: 'Cafe de Coral'
+                },
+                {
+                    imageUrl: require('../../assets/splash.png'),
+                    title: 'Kai Kee'
+                },
+            ]
+        }
+    }
+
+    render(){
+        return (
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={
+                        ({item}) => <MerchantCard 
+                        imageUri={item.imageUrl}
+                        title={item.title}/>
+                    }
+                />
+            </View>
+        )
+    }
 }
 export default Food
 
